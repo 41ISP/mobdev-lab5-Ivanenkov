b9478deed534555355e5259989c7d6362630582f
DaniilIvanenkov <132118406+DaniilIvanenkov@users.noreply.github.com>
2025-02-04 08:03:02 +0000

    0.1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,23 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
-
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [counter, setCounter]= useState(0)
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const handleclick = () =>{
+    setCounter((value) => value+1)
 
+  }
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -29,11 +30,23 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      
+          <View style={styles.container}>
+            <TouchableOpacity onPress={handleclick}>-
+            </TouchableOpacity>
+            <Text style={styles.text}>{counter}</Text>
+          </View>
+        
     </ThemeProvider>
   );
 }
+const styles = StyleSheet.create({
+  container:{
+     marginTop:100,
+     flexDirection:"row"
+  },
+  text:{
+    color:"#ff0000",
+    fontSize: 24,
+  }
+});
